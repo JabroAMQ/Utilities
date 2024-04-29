eb2bfbec7bc5ce8bb93f21d1cddf1c551ca59a51
JabroAMQ <jabrothegamer@gmail.com>
2024-04-29 11:10:51 +0200

    documentation stuff

--- AMQ/RoomNameAutosetters/AMQRoomNameFromSavesSettings.user.js
+++ AMQ/RoomNameAutosetters/AMQRoomNameFromSavesSettings.user.js
@@ -8,6 +8,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
 // @grant        none
 // @require      https://raw.githubusercontent.com/joske2865/AMQ-Scripts/master/common/amqScriptInfo.js
+// @downloadURL  https://github.com/JabroAMQ/Utilities/blob/main/AMQ/RoomNameAutosetters/AMQRoomNameFromSavesSettings.user.js
+// @updateURL    https://github.com/JabroAMQ/Utilities/blob/main/AMQ/RoomNameAutosetters/AMQRoomNameFromSavesSettings.user.js
 // ==/UserScript==
 
 // Do not load the script in the login page
@@ -37,12 +39,12 @@ function roomNameFromSavedSettings() {
 AMQ_addScriptData({
     name: 'AMQ Room Name from Saved Settings',
     author: 'Jabro',
-    link: 'placeholder',
+    link: 'https://github.com/JabroAMQ/Utilities/blob/main/AMQ/RoomNameAutosetters/AMQRoomNameFromSavesSettings.user.js',
     version: 0.0,
     description: `
         <div>
             <p>Automatically set the room name to the one stored when loading settings from saved settings:</p>
-            <img src='placeholder' alt='placeholder'>
+            <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/RoomNameAutosetters/images/roomNameFromSavedSettings.png' alt='roomNameDromSavedSettingsPNG'>
         </div>
     `
 });
