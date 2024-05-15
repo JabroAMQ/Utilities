6b178c44a1710775c7a3d4210c3c520054985f7f
JabroAMQ <jabrothegamer@gmail.com>
2024-05-15 09:19:07 +0200

    v0.4.1: Refactoring for easier UI modifications

--- AMQ/MessagesAutosender/AMQMessagesAutosender.user.js
+++ AMQ/MessagesAutosender/AMQMessagesAutosender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Messages Autosender
 // @namespace    https://github.com/JabroAMQ/
-// @version      0.4
+// @version      0.4.1
 // @description  Allow the user to store some messages and autosend them when clicked
 // @author       Jabro
 // @match        https://animemusicquiz.com/*
@@ -13,55 +13,26 @@
 // @updateURL    https://github.com/JabroAMQ/Utilities/blob/main/AMQ/MessagesAutosender/AMQMessagesAutosender.user.js
 // ==/UserScript==
 
-const VERSION = '0.4';    // Documentation purposes only. Make sure this value matches with the @version one from the userscript header
-
-AMQ_addScriptData({
-    name: 'AMQ Messages Autosender',
-    author: 'Jabro',
-    link: 'https://github.com/JabroAMQ/Utilities/blob/main/AMQ/MessagesAutosender/AMQMessagesAutosender.user.js',
-    version: VERSION,
-    description: `
-        <p>Allow the player to save custom messages and, once they are clicked, autosend them to game chat</p>
-        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/ExampleGIF.gif' alt='ExampleGIF'>
-        <p>You can open the window from an in-game button:</p>
-        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/OpenButton.png' alt='OpenButton'>
-        <p>Or from the options container:</p>
-        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/OptionsContainer.png' alt='OptionsContainer'>
-    `
-});
-
-/*
-TODO list:
-
-- Prettify the window stuff
-*/
-
-///////////////////////////////////////////////////////////////////
-////////////////////////  INITIALIZATION   ////////////////////////
-///////////////////////////////////////////////////////////////////
-
+const VERSION = '0.4.1';
+const DELAY = 500;
 const MAX_MESSAGE_LENGTH = 150;
 let savedMessagesWindow;
 let savedMessages = [];
 
 
-// Do not load the script in the login page
 if (document.getElementById('loginPage'))
     return;
 
-// Wait until the LOADING... screen is hidden and load script
 let loadInterval = setInterval(() => {
     if ($('#loadingScreen').hasClass('hidden')) {
         clearInterval(loadInterval);
-        loadSavedMessages();
+        loadMessages();
         createSavedMessagesWindow();
+        createSavedMessagesWindowButton();
+        addSavedMessagesWindowToOptionsContainer();
     }
-}, 500);
+}, DELAY);
 
-
-///////////////////////////////////////////////////////////////////
-/////////////////////////  WINDOW STUFF   /////////////////////////
-///////////////////////////////////////////////////////////////////
 
 function createSavedMessagesWindow() {
     savedMessagesWindow = new AMQWindow({
@@ -76,12 +47,10 @@ function createSavedMessagesWindow() {
 
     addNewMessagePanel();
     addSavedMessagesPanel();
-    addSavedMessagesWindowToOptions();
-    setupSavedMessagesWindowButton();
 }
 
-
 function addNewMessagePanel() {
+    // Create the "New Message" panel
     savedMessagesWindow.addPanel({
         id: 'addNewMessagePanel',
         width: 1,
@@ -89,42 +58,34 @@ function addNewMessagePanel() {
         scrollable: { x: true, y: false }
     });
 
-    savedMessagesWindow.panels[0].panel.append(createSeparatorLine());
+    // Add a panel-separator to the "New Message" panel
+    const separator = document.createElement('div');
+    separator.id = 'panelSeparator';
+    const newMessagePanel = savedMessagesWindow.panels[0].panel
+    newMessagePanel.append(separator);
 
-    // Create a container leaving a 5% padding in all directions
+    // Create a content container for the "New Message" panel
     const container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
-    container.style.width = '100%';
-    container.style.height = '90%';
+    container.id = 'newMessageContainer';
 
-    // Add an input textfield inside the container allowing the user to write the messages they want to save
+    // Create a text input and add it to the "New Message" container
     const messageInput = document.createElement('input');
-    messageInput.type = 'text';
     messageInput.id = 'newMessageTextField';
+    messageInput.type = 'text';
     messageInput.placeholder = 'Write a message here...';
-    messageInput.style.color = 'black';
-    messageInput.style.width = '75%';
-    messageInput.style.height = '65%'
-    messageInput.style.marginLeft = '5%';
-    messageInput.style.marginRight = '5%';
     container.appendChild(messageInput);
 
-    // Add a button to the container as well to save the message
+    // Create the "Save" button and add it to the "New Message" container
     const saveButton = document.createElement('button');
+    saveButton.id = 'newMessageSaveButton';
     saveButton.innerHTML = 'Save';
     saveButton.onclick = saveMessage;
-    saveButton.style.color = 'black';
-    saveButton.style.width = '15%';
-    saveButton.style.height = '65%';
-    saveButton.style.marginRight = '5%';
     container.appendChild(saveButton);
 
-    savedMessagesWindow.panels[0].panel.append(container);
-    savedMessagesWindow.panels[0].panel.append(createSeparatorLine());
+    // Add the container to the "New Message" panel as well as another panel-separator
+    newMessagePanel.append(container);
+    newMessagePanel.append(separator);
 }
-
 
 function addSavedMessagesPanel() {
     savedMessagesWindow.addPanel({
@@ -140,7 +101,6 @@ function addSavedMessagesPanel() {
 }
 
 function addMessageContainerToPanel(textFieldValue) {    
-    // Do not add the message if no message was introduced...
     if (textFieldValue.length <= 0)
         return;
 
@@ -148,76 +108,85 @@ function addMessageContainerToPanel(textFieldValue) {
     if (textFieldValue.length > MAX_MESSAGE_LENGTH)
         textFieldValue = textFieldValue.substring(0, MAX_MESSAGE_LENGTH);
     
-    // Create a new container for each saved message
+    // Create a content container for the saved message
     const container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
-    container.style.width = '100%';
-    container.style.height = '10%';
+    container.id = 'savedMessageContainer';
 
-    // Add a div to display the saved message text
-    const savedMessageDiv = document.createElement('div');
-    savedMessageDiv.innerHTML = textFieldValue;
-    savedMessageDiv.title = textFieldValue;
-    savedMessageDiv.style.color = 'black';
-    savedMessageDiv.style.width = '75%';
-    savedMessageDiv.style.marginLeft = '5%';
-    savedMessageDiv.style.marginRight = '5%';
-    savedMessageDiv.style.overflow = 'hidden';
-    savedMessageDiv.style.textOverflow = 'ellipsis';
-    savedMessageDiv.style.backgroundColor = 'white';
-    container.appendChild(savedMessageDiv);
+    // Display the saved message as a button and add it to the container
+    const savedMessage = document.createElement('button');
+    savedMessage.id = 'savedMessage';
+    savedMessage.innerHTML = textFieldValue;
+    savedMessage.title = textFieldValue;
+    savedMessage.onclick = function() {
+        sendMessage(textFieldValue);
+    };
+    container.appendChild(savedMessage);
 
-    // Add a delete button to remove the saved message
+    // Create a delete button to remove the saved message and add it to the container
     const deleteButton = document.createElement('button');
+    deleteButton.id = 'deleteSavedMessageButton';
     deleteButton.innerHTML = 'Delete';
     deleteButton.onclick = function (event) {
         event.stopPropagation();
         deleteMessage(container, textFieldValue);
     };
-    deleteButton.style.color = 'black';
-    deleteButton.style.width = '15%';
-    deleteButton.style.marginRight = '5%';
-    container.appendChild(deleteButton);
-
-    // Send the message from the container to game chat when clicked
-    container.onclick = function() { sendMessageToGameChat(textFieldValue); };
+    container.appendChild(deleteButton);    
 
     // Append the container to the 'savedMessagesPanel' panel
-    savedMessagesWindow.panels[1].panel.append(container);
+    const savedMessagesPanel = savedMessagesWindow.panels[1].panel
+    savedMessagesPanel.append(container);
 }
 
 
-function createSeparatorLine() {
-    const separator = document.createElement('div');
-    separator.style.width = '100%';
-    separator.style.height = '2px';
-    separator.style.backgroundColor = 'black';
-    return separator;
+function saveMessage() {
+    let textFieldValue = document.getElementById('newMessageTextField').value;
+    textFieldValue = translateShortcodeToUnicode(textFieldValue).text;          // emojis
+    addMessageContainerToPanel(textFieldValue);
+
+    // Log the changes for future sessions
+    savedMessages.push(textFieldValue);
+    storeMessages();
+}
+
+function deleteMessage(container, textFieldValue) {
+    // Remove the container from the panel
+    container.parentNode.removeChild(container)
+
+    // Log the changes for future sessions
+    let index = savedMessages.indexOf(textFieldValue);
+    savedMessages.splice(index, 1);
+    storeMessages();
+}
+
+function sendMessage(content) {
+    let gameChatPage = document.getElementById('gameChatPage');
+    if (!gameChatPage || gameChatPage.classList.contains('hidden')) {
+        showErrorModal();
+        return; 
+    }
+
+    let chatInput = document.getElementById('gcInput');
+    chatInput.value = content;
+    let enterEvent = new KeyboardEvent('keypress', { key: 'Enter', keyCode: 13 });
+    chatInput.dispatchEvent(enterEvent);
+}
+
+function showErrorModal() {
+    Swal.fire({
+        title: 'Unable to send the message',
+        text: 'You can\'t send a message if you are not in a lobby.',
+        showConfirmButton: true,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK'
+      });
 }
 
 
-function addSavedMessagesWindowToOptions() {
-    // Add a new list item to the optionsContainer
-    let optionsContainer = document.getElementById('optionsContainer');
-
-    let savedMessagesOption = document.createElement('li');
-    savedMessagesOption.className = 'clickAble';
-    savedMessagesOption.textContent = 'Saved Messages';
-    savedMessagesOption.onclick = openSavedMessagesWindow;
-
-    // Insert the new list item after the "Installed Userscripts" one
-    let installedUserscriptsLi = optionsContainer.querySelector('ul [data-target="#installedModal"]');
-    installedUserscriptsLi.parentNode.insertBefore(savedMessagesOption, installedUserscriptsLi.nextSibling);
-}
-
-
-function setupSavedMessagesWindowButton() {
+function createSavedMessagesWindowButton() {
     const buttonWidth = 30;
     const buttonMarginRight = 5;
 
-    let savedMessagesWindowButton = $(`
+    const savedMessagesWindowButton = $(`
         <div id='savedMessagesWindowButton' class='clickAble qpOption'>
             <i aria-hidden='true' class='fa fa-comments qpMenuItem'></i>
         </div>`)
@@ -233,83 +202,46 @@ function setupSavedMessagesWindowButton() {
 			trigger: 'hover'
 	    });
 
-    let currentWidth = $('#qpOptionContainer').width();
-    let extraWidth = buttonWidth + buttonMarginRight;
+    const currentWidth = $('#qpOptionContainer').width();
+    const extraWidth = buttonWidth + buttonMarginRight;
     $('#qpOptionContainer').width(currentWidth + extraWidth);
     $('#qpOptionContainer > div').append(savedMessagesWindowButton);
 }
 
+function addSavedMessagesWindowToOptionsContainer() {
+    const optionsContainer = document.getElementById('optionsContainer');
+    
+    // Add a new list item to the optionsContainer
+    const savedMessagesOption = document.createElement('li');
+    savedMessagesOption.className = 'clickAble';
+    savedMessagesOption.textContent = 'Saved Messages';
+    savedMessagesOption.onclick = openSavedMessagesWindow;
 
-function showErrorModal() {
-    Swal.fire({
-        title: 'Unable to send the message',
-        text: 'You can\'t send a message if you are not in a lobby.',
-        showConfirmButton: true,
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'OK'
-      });
+    // Insert the new list item after the "Installed Userscripts" one
+    const installedUserscriptsLi = optionsContainer.querySelector('ul [data-target="#installedModal"]');
+    installedUserscriptsLi.parentNode.insertBefore(savedMessagesOption, installedUserscriptsLi.nextSibling);
 }
-
-
-///////////////////////////////////////////////////////////////////
-//////////////////////////  FUNCTIONALITY   ///////////////////////
-///////////////////////////////////////////////////////////////////
 
 function openSavedMessagesWindow() {
-    // Open the window if not visible and close it otherwise
-    savedMessagesWindow.isVisible() ? savedMessagesWindow.close() : savedMessagesWindow.open()
-}
-
-function sendMessageToGameChat(content) {
-    // Make sure we are in game
-    let gameChatPage = document.getElementById('gameChatPage');
-    if (!gameChatPage || gameChatPage.classList.contains('hidden')) {
-        showErrorModal();
-        return; 
-    }
-
-    // Paste the content into the chat input box
-    let chatInput = document.getElementById('gcInput');
-    chatInput.value = content;
-    
-    // Pretend "enter keypress" (send) event
-    let enterEvent = new KeyboardEvent('keypress', { key: 'Enter', keyCode: 13 });
-    chatInput.dispatchEvent(enterEvent);
+    savedMessagesWindow.isVisible() ? savedMessagesWindow.close() : savedMessagesWindow.open();
 }
 
 
-function saveMessage() {
-    let textFieldValue = document.getElementById('newMessageTextField').value;
-
-    // Find and convert emojis
-    textFieldValue = translateShortcodeToUnicode(textFieldValue).text;
-
-    addMessageContainerToPanel(textFieldValue);
-
-    // Log the changes for future sessions
-    savedMessages.push(textFieldValue);
-    storeSavedMessages();
+// Cookies stuff: https://stackoverflow.com/a/24103596/20214407
+function loadMessages() {
+    const savedMessagesString = getCookie('savedMessages');
+    if (savedMessagesString)
+        savedMessages = JSON.parse(savedMessagesString);
 }
 
-function deleteMessage(container, textFieldValue) {
-    // Remove the container from the panel
-    container.parentNode.removeChild(container)
-
-    // Log the changes for future sessions
-    let index = savedMessages.indexOf(textFieldValue);
-    savedMessages.splice(index, 1);
-    storeSavedMessages();
+function storeMessages() {
+    const messagesString = JSON.stringify(savedMessages);
+    setCookie('savedMessages', messagesString, 9999);
 }
 
-
-///////////////////////////////////////////////////////////////////
-//////////////////////////  COOKIES STUFF   ///////////////////////
-///////////////////////////////////////////////////////////////////
-
-// https://stackoverflow.com/a/24103596/20214407
 function getCookie(name) {
-    let nameEQ = name + '=';
-    let ca = document.cookie.split(';');
+    const nameEQ = name + '=';
+    const ca = document.cookie.split(';');
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) === ' ')
@@ -323,7 +255,7 @@ function getCookie(name) {
 function setCookie(name, value, days) {
     let expires = '';
     if (days) {
-        let date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
         expires = '; expires=' + date.toUTCString();
     }
@@ -331,13 +263,79 @@ function setCookie(name, value, days) {
 }
 
 
-function loadSavedMessages() {
-    let savedMessagesString = getCookie('savedMessages');
-    if (savedMessagesString)
-        savedMessages = JSON.parse(savedMessagesString);
-}
+AMQ_addStyle(`
+    /* "New Message" panel */
+    #newMessageContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 90%;
+    }
 
-function storeSavedMessages() {
-    let messagesString = JSON.stringify(savedMessages);
-    setCookie('savedMessages', messagesString, 9999);
-}
+    #newMessageTextField {
+        color: black;
+        width: 75%;
+        height: 65%;
+        margin-left: 5%;
+        margin-right: 5%;
+    }
+
+    #newMessageSaveButton {
+        color: black;
+        width: 15%;
+        height: 65%;
+        margin-right: 5%;
+    }
+
+
+    /* Panel separator */
+    #panelSeparator {
+        width: 100%;
+        height: 2px;
+        background-color: black;
+    }
+
+    
+    /* "Saved Message" panel */
+    #savedMessageContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 10%;
+    }
+
+    #savedMessage {
+        color: black;
+        width: 75%;
+        margin-left: 5%;
+        margin-right: 5%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        background-color: white;
+    }
+
+    #deleteSavedMessageButton {
+        color: black;
+        width: 15%;
+        margin-right: 5%;
+    }
+
+`);
+
+
+AMQ_addScriptData({
+    name: 'AMQ Messages Autosender',
+    author: 'Jabro',
+    link: 'https://github.com/JabroAMQ/Utilities/blob/main/AMQ/MessagesAutosender/AMQMessagesAutosender.user.js',
+    version: VERSION,
+    description: `
+        <p>Allow the player to save custom messages and, once they are clicked, autosend them to game chat</p>
+        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/ExampleGIF.gif' alt='ExampleGIF'>
+        <p>You can open the window from an in-game button:</p>
+        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/OpenButton.png' alt='OpenButton'>
+        <p>Or from the options container:</p>
+        <img src='https://github.com/JabroAMQ/Utilities/raw/main/AMQ/MessagesAutosender/images/OptionsContainer.png' alt='OptionsContainer'>
+    `
+});
