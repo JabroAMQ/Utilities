034555a29b67d16c0f49c0592ef5e12e43ba84cf
JabroAMQ <jabrothegamer@gmail.com>
2026-05-21 15:24:26 +0200

    download and update links

--- AMQ/ShowSongListShortcut/AMQShowSongListShortcut.js
+++ AMQ/ShowSongListShortcut/AMQShowSongListShortcut.js
@@ -7,6 +7,8 @@
 // @match        https://*.animemusicquiz.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animemusicquiz.com
 // @grant        none
+// @downloadURL  https://github.com/JabroAMQ/Utilities/blob/main/AMQ/ShowSongListShortcut/AMQShowSongListShortcut.user.js
+// @updateURL    https://github.com/JabroAMQ/Utilities/blob/main/AMQ/ShowSongListShortcut/AMQShowSongListShortcut.user.js
 // ==/UserScript==
 
 const DELAY = 500;
